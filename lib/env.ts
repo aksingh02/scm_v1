b14351f80007c30ba3dc38ea0181a23557ebc258
dev3-talcyber <dev3@talcyber.com>
2025-09-17 15:31:24 +0530

    added env

---
+++ lib/env.ts
@@ -0,0 +1,11 @@
+export const env = {
+
+  ////// local:
+  baseUrl: "http://localhost:8081/api",
+
+  ////// production:
+  // baseUrl: "https://api.sylphcorpsmedia.com/api",
+
+  apiTimeOut: 10000,
+  apiKey: "sk_prod_abc123def456ghi789jkl012mno345pqr"
+}
