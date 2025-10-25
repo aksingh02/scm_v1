4cf55a2cbde342e5c0344c873e027201fa773721
dev3-talcyber <dev3@talcyber.com>
2025-10-26 02:07:54 +0530

    have updated robot.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,8 +7,8 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
     sitemap: [
-      "https://media.sylphcorps.com/sitemap.xml",
-      "https://media.sylphcorps.com/server-sitemap.xml",
+      "https://sylphcorpsmedia.com/sitemap.xml",
+      "https://sylphcorpsmedia.com/server-sitemap.xml",
     ],
   }
 }
